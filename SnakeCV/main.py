50ff006eb9ffdf02979b25d3a1a4927b664a5d35
Rahul <rahulkotr1@gmail.com>
2022-03-14 16:36:33 +0530

    Checking cams

--- SnakeCV/main.py
+++ SnakeCV/main.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2
+import mediapipe
 
 cap=cv2.VideoCapture(0)
 
 cap.set(3,1920)
 cap.set(4.1080)
+detector=HandDetector(detectionCon=0.8,maxHands=1)
+while(True):
+    success,img=cap.read()
+    hands,img=detector.findHands(img)
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
