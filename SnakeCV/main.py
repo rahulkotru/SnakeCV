2f1fe90ab9c85bff6008c9207999df268366de1d
Rahul <rahulkotr1@gmail.com>
2022-03-14 18:10:32 +0530

    Incoporated hand detection

--- SnakeCV/main.py
+++ SnakeCV/main.py
@@ -6,10 +6,16 @@ cap=cv2.VideoCapture(0)
 
 cap.set(3,1920)
 cap.set(4,1080)
-detector=HandDetector(detectionCon=0.8,maxHands=1)
+detector=HandDetector(detectionCon=0.8,maxHands=2)
 while(True):
     success,img=cap.read()
     img=cv2.flip(img,1)
     hands,img=detector.findHands(img)
+
+    if hands:
+        lmList=hands[0]['lmList']
+        pointIndex=lmList[8][0:2]
+        cv2.circle(img,pointIndex,20,(200,0,200),cv2.FILLED)
+
     cv2.imshow('Image',img)
     cv2.waitKey(1)
