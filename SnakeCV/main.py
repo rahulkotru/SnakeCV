7db074e2e580e2c0f5b28aee943bc79a5ad9e604
Rahul <rahulkotr1@gmail.com>
2022-03-14 16:48:18 +0530

    Add hand detection

--- SnakeCV/main.py
+++ SnakeCV/main.py
@@ -6,8 +6,10 @@ cap=cv2.VideoCapture(0)
 
 cap.set(3,1920)
 cap.set(4,1080)
-#detector=HandDetector(detectionCon=0.8,maxHands=1)
+detector=HandDetector(detectionCon=0.8,maxHands=1)
 while(True):
     success,img=cap.read()
+    img=cv2.flip(img,1)
+    hands,img=detector.findHands(img)
     cv2.imshow('Image',img)
     cv2.waitKey(1)
