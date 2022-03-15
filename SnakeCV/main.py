30a47857b240b02745670a284bb4a5c2a785f86a
Rahul <rahulkotr1@gmail.com>
2022-03-15 21:42:12 +0530

    Write to game

--- SnakeCV/main.py
+++ SnakeCV/main.py
@@ -19,7 +19,12 @@ class SnakeGame:
         self.lengths.append(distance)
         self.currentLength+=distance
         self.previousHead=cx,cy
-        
+
+
+        for i,points in enumerate(self.points):
+            if i!=0:
+                cv2.line(imgMain,self.points[i-1],self.points[i],(0,0,255),20)
+        cv2.circle(img,self.points[-1],20,(200,0,200),cv2.FILLED)
 
 
 
