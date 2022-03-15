1a158ba0d4036337209960abfc79dc7805b51757
Rahul <rahulkotr1@gmail.com>
2022-03-15 21:45:03 +0530

    Create game object

--- SnakeCV/main.py
+++ SnakeCV/main.py
@@ -25,6 +25,7 @@ class SnakeGame:
             if i!=0:
                 cv2.line(imgMain,self.points[i-1],self.points[i],(0,0,255),20)
         cv2.circle(img,self.points[-1],20,(200,0,200),cv2.FILLED)
+        return imgMain
 
 
 
@@ -33,6 +34,7 @@ cap=cv2.VideoCapture(0)
 cap.set(3,1920)
 cap.set(4,1080)
 detector=HandDetector(detectionCon=0.8,maxHands=2)
+game=SnakeGame()
 while(True):
     success,img=cap.read()
     img=cv2.flip(img,1)
@@ -41,6 +43,7 @@ while(True):
     if hands:
         lmList=hands[0]['lmList']
         pointIndex=lmList[8][0:2]
+        img=game.update(img,pointIndex)
         cv2.circle(img,pointIndex,20,(200,0,200),cv2.FILLED)
 
     cv2.imshow('Image',img)
