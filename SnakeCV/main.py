b28cb6e0ad5b867bd8f1fd2d119ad77d61dfadd2
Rahul <rahulkotr1@gmail.com>
2022-03-15 21:59:16 +0530

    Draw snake

--- SnakeCV/main.py
+++ SnakeCV/main.py
@@ -21,11 +21,19 @@ class SnakeGame:
         self.currentLength+=distance
         self.previousHead=cx,cy
 
-
-        for i,points in enumerate(self.points):
-            if i!=0:
-                cv2.line(imgMain,self.points[i-1],self.points[i],(0,0,255),20)
-        cv2.circle(imgMain,self.points[-1],20,(200,0,200),cv2.FILLED)
+        if self.currentLength>self.allowedLength:
+            for i,length in enumerate(self.lengths):
+                self.currentLength-=length
+                self.lengths.pop(i)
+                self.points.pop(i)
+                if self.currentLength<self.allowedLength:
+                    break
+        if self.points:
+            
+            for i,points in enumerate(self.points):
+                if i!=0:
+                    cv2.line(imgMain,self.points[i-1],self.points[i],(0,0,255),20)
+            cv2.circle(imgMain,self.points[-1],20,(200,0,200),cv2.FILLED)
         return imgMain
 
 
